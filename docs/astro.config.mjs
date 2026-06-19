e1fcdd49cafe20bb6135d061e8dc1d1a08d6d3cb
alan7383 <alan.mussot@outlook.fr>
2026-06-20 01:58:08 +0200

    ci: add GitHub Actions workflow to deploy docs to GitHub Pages

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -4,6 +4,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://alan7383.github.io',
+	base: '/playify',
 	integrations: [
 		starlight({
 			title: 'Playify',
